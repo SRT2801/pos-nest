19c2667d43a1314fa86d50403078bae3a09c2202
SRT DEV <sahirruiz4@gmail.com>
2026-02-07 20:40:41 -0500

    feat: Implement ProductsService with CRUD operations and category validation

---
+++ src/products/products.service.ts
@@ -0,0 +1,61 @@
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { FindManyOptions, Repository } from 'typeorm';
+import { Category } from 'src/categories/entities/category.entity';
+import { NotFoundException } from '@nestjs/common';
+
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const category = await this.categoryRepository.findOneBy({
+      id: createProductDto.categoryId,
+    });
+    if (!category) {
+      let errors: string[] = [];
+      errors.push('Category not found');
+      throw new NotFoundException(errors);
+    }
+
+    return this.productRepository.save({
+      ...createProductDto,
+      category,
+    });
+  }
+
+  async findAll(categoryId: number | null, page: number) {
+    const options: FindManyOptions<Product> = {
+      relations: { category: true },
+      order: { id: 'DESC' },
+      take: page,
+    };
+
+    if (categoryId) {
+      options.where = { category: { id: categoryId } };
+    }
+
+    const [products, total] =
+      await this.productRepository.findAndCount(options);
+
+    return { products, total };
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
