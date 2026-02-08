851e64ce821bbf304e7cf11df5f6902846108f82
SRT DEV <sahirruiz4@gmail.com>
2026-02-08 14:14:51 -0500

    feat: Update ProductsController to enhance query handling and add ID validation

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -12,6 +12,7 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { GetProductsQueryDto } from './dto/get-product.dto';
+import { IdValidationPipe } from 'src/common/pipes/id-validation/id-validation.pipe';
 
 @Controller('products')
 export class ProductsController {
@@ -25,23 +26,24 @@ export class ProductsController {
   @Get()
   findAll(@Query() query: GetProductsQueryDto) {
     const category = query.category_id ? query.category_id : null;
-    const page = query.page ? query.page : 10;
+    const take = query.take ? query.take : 10;
+    const skip = query.skip ? query.skip : 0;
 
-    return this.productsService.findAll(category, page);
+    return this.productsService.findAll(category, take, skip);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', IdValidationPipe) id: string) {
     return this.productsService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  update(@Param('id', IdValidationPipe) id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', IdValidationPipe) id: string) {
     return this.productsService.remove(+id);
   }
 }
