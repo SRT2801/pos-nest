78a46ef99c28598d861339eb3db3860560cff0db
SRT DEV <sahirruiz4@gmail.com>
2026-02-08 19:11:38 -0500

    feat: Implement CouponsService for coupon management

---
+++ src/coupons/coupons.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateCouponDto } from './dto/create-coupon.dto';
+import { UpdateCouponDto } from './dto/update-coupon.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Coupon } from './entities/coupon.entity';
+import { read } from 'fs';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class CouponsService {
+  constructor(
+    @InjectRepository(Coupon)
+    private readonly couponRepository: Repository<Coupon>,
+  ) {}
+
+  create(createCouponDto: CreateCouponDto) {
+    return this.couponRepository.save(createCouponDto);
+  }
+
+  findAll() {
+    return this.couponRepository.find();
+  }
+
+  async findOne(id: number) {
+    const coupon = await this.couponRepository.findOneBy({ id });
+    if (!coupon) {
+      throw new NotFoundException('Coupon not found');
+    }
+    return coupon;
+  }
+
+  async update(id: number, updateCouponDto: UpdateCouponDto) {
+    const coupon = await this.findOne(id);
+    Object.assign(coupon, updateCouponDto);
+
+    return this.couponRepository.save(coupon);
+  }
+
+  async remove(id: number) {
+    const coupon = await this.findOne(id);
+    await this.couponRepository.remove(coupon);
+    return { message: 'Coupon removed successfully' };
+  }
+}
