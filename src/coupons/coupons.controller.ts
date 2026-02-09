99c77af0f44e109318cea48bb506bd73e1cc3fb3
SRT DEV <sahirruiz4@gmail.com>
2026-02-08 19:11:15 -0500

    feat: Implement CouponsController for coupon management

---
+++ src/coupons/coupons.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { CouponsService } from './coupons.service';
+import { CreateCouponDto } from './dto/create-coupon.dto';
+import { UpdateCouponDto } from './dto/update-coupon.dto';
+import { IdValidationPipe } from 'src/common/pipes/id-validation/id-validation.pipe';
+
+@Controller('coupons')
+export class CouponsController {
+  constructor(private readonly couponsService: CouponsService) {}
+
+  @Post()
+  create(@Body() createCouponDto: CreateCouponDto) {
+    return this.couponsService.create(createCouponDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.couponsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', IdValidationPipe) id: string) {
+    return this.couponsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id',IdValidationPipe) id: string, @Body() updateCouponDto: UpdateCouponDto) {
+    return this.couponsService.update(+id, updateCouponDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.couponsService.remove(+id);
+  }
+}
