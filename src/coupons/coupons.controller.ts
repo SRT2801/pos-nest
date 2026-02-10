3823eacbbc0a13de00f9d23426bf6934be367e9f
SRT DEV <sahirruiz4@gmail.com>
2026-02-10 12:22:23 -0500

    feat: Add applyCoupon endpoint to CouponsController for coupon application

--- src/coupons/coupons.controller.ts
+++ src/coupons/coupons.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { CouponsService } from './coupons.service';
 import { CreateCouponDto } from './dto/create-coupon.dto';
 import { UpdateCouponDto } from './dto/update-coupon.dto';
 import { IdValidationPipe } from 'src/common/pipes/id-validation/id-validation.pipe';
+import { ApplyCouponDto } from './dto/apply-coupon.dto';
 
 @Controller('coupons')
 export class CouponsController {
@@ -24,12 +35,21 @@ export class CouponsController {
   }
 
   @Patch(':id')
-  update(@Param('id',IdValidationPipe) id: string, @Body() updateCouponDto: UpdateCouponDto) {
+  update(
+    @Param('id', IdValidationPipe) id: string,
+    @Body() updateCouponDto: UpdateCouponDto,
+  ) {
     return this.couponsService.update(+id, updateCouponDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.couponsService.remove(+id);
+  }
+
+  @Post('/apply-coupon')
+  @HttpCode(HttpStatus.OK)
+  applyCoupon(@Body() applyCouponDto: ApplyCouponDto) {
+    return this.couponsService.applyCoupon(applyCouponDto.name);
   }
 }
