82081ac5f970cb978cc7d84c5568926e4c3b0ed6
SRT DEV <sahirruiz4@gmail.com>
2026-02-12 12:13:52 -0500

    feat: Add products query option to findOne method in CategoriesService and update CategoriesController

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -2,24 +2,29 @@ import { Category } from '../../categories/entities/category.entity';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class Product {
-    
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({type: 'varchar', length: 60})
-    name: string;
+  @Column({ type: 'varchar', length: 60 })
+  name: string;
 
-    @Column({type: 'varchar', length: 120, nullable: true, default: 'default.svg'})
-    image: string;
+  @Column({
+    type: 'varchar',
+    length: 120,
+    nullable: true,
+    default: 'default.svg',
+  })
+  image: string;
 
-    @Column({type: 'decimal'})
-    price: number;
+  @Column({ type: 'decimal' })
+  price: number;
 
-    @Column({type: 'int'})
-    inventory: number;
+  @Column({ type: 'int' })
+  inventory: number;
 
-    @ManyToOne (() => Category)
-    category: Category;
+  @Column()
+  categoryId: number;
 
-
+  @ManyToOne(() => Category)
+  category: Category;
 }
