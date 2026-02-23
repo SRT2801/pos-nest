9f76ddbb1858bfe3bf12ff5f711051a1a388f35d
SRT DEV <sahirruiz4@gmail.com>
2026-02-23 14:18:11 -0500

    feat: Enable CORS in main application setup

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
