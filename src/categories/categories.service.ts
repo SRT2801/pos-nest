adf65e437bc57c302d1a0bfca2a6ac6a26aa999d
SRT DEV <sahirruiz4@gmail.com>
2026-03-25 00:15:02 -0500

    refactor: extend StoreScopedService and update scoped methods

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,23 +1,28 @@
-import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
-import { Category } from './entities/category.entity';
+import { CreateCategoryDto } from './dto/create-category.dto.js';
+import { UpdateCategoryDto } from './dto/update-category.dto.js';
+import { Category } from './entities/category.entity.js';
 import { FindManyOptions, Repository } from 'typeorm';
+import { StoreScopedService } from '../common/services/store-scoped.service.js';
+import { StoreContextService } from '../common/cls/store-context.service.js';
 
 @Injectable()
-export class CategoriesService {
+export class CategoriesService extends StoreScopedService<Category> {
   constructor(
     @InjectRepository(Category)
     private readonly categoryRepository: Repository<Category>,
-  ) {}
+    storeContext: StoreContextService,
+  ) {
+    super(categoryRepository, storeContext);
+  }
 
   create(createCategoryDto: CreateCategoryDto) {
-    return this.categoryRepository.save(createCategoryDto);
+    return this.scopedSave(createCategoryDto as Partial<Category>);
   }
 
   findAll() {
-    return this.categoryRepository.find();
+    return this.scopedFind();
   }
 
   async findOne(id: number, products?: boolean) {
@@ -28,15 +33,15 @@ export class CategoriesService {
     if (products) {
       options.relations = {
         products: true,
-      },
-      options.order ={
+      };
+      options.order = {
         products: {
           id: 'ASC',
         },
-      }
+      };
     }
 
-    const category = await this.categoryRepository.findOne(options);
+    const category = await this.scopedFindOne(options);
     if (!category) {
       throw new NotFoundException('Category not found');
     }
