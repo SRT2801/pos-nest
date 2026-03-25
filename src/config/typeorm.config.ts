0fad40c4154bb1401ffb1adcbc21518dc6fe4b56
SRT DEV <sahirruiz4@gmail.com>
2026-03-25 00:19:43 -0500

    refactor: migrate TypeORM config to DataSource and env variables

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,6 +1,10 @@
 import { ConfigService } from '@nestjs/config';
 import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { join } from 'path';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const typeOrmConfig = (
   configService: ConfigService,
@@ -16,5 +20,25 @@ export const typeOrmConfig = (
   },
   logging: false,
   entities: [join(__dirname, '..', '**', '*.entity.{ts,js}')],
-  synchronize: true,
+  migrations: [join(__dirname, '..', 'database', 'migrations', '*.{ts,js}')],
+  synchronize: false,
 });
+
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASS,
+  database: process.env.DATABASE_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  logging: false,
+  entities: [join(__dirname, '..', '**', '*.entity.{ts,js}')],
+  migrations: [join(__dirname, '..', 'database', 'migrations', '*.{ts,js}')],
+  synchronize: false,
+};
+
+export default new DataSource(dataSourceOptions);
