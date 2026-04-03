202f884b3605551211b70ed49181e755f62f3fd9
SRT DEV <sahirruiz4@gmail.com>
2026-04-02 22:28:59 -0500

    feat: update password validation rules in RegisterStoreDto to enforce minimum length and complexity

--- src/auth/dto/register-store.dto.ts
+++ src/auth/dto/register-store.dto.ts
@@ -14,7 +14,13 @@ export class RegisterStoreDto {
 
   @IsString()
   @IsNotEmpty()
-  @MinLength(6)
+  @MinLength(10, {
+    message: 'Password must be at least 10 characters long',
+  })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/, {
+    message:
+      'Password must include at least one uppercase letter, one lowercase letter and one number',
+  })
   password: string;
 
   @IsString()
